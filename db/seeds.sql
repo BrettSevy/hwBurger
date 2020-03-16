f45669a1b30bc9a3ec31b0ec4427aeaf8c1fccd7
Brett Sevy <justbrettt@gmail.com>
2020-03-15 22:51:54 -0500

    ugh heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,12 +2,29 @@
 INSERT INTO 
 	burgers (burger_name, devoured)
 VALUES 
-	("Into Mordor", false),
-    ("Hot Tamale", false),
-    ("Itty Bitty", true),
-    ("Cry Me a River", true),
-    ("Goodbye Vegan Diet", false),
-    ("Go Home Hungry", true);
+	("Luger Burger", false),
+    ("Le Pigeon Burger", false),
+    ("Double Animal Style", true),
+    ("Whiskey King Burger", true),
+    ("The Company Burger", false),
+    ("Dyer's Deep Fried Burger", false),
+    ("Au Chevel Cheeseburger", false),
+    ("Flip Burger", true),
+    ("Umami Burger", true),
+    ("Buckhorn Burger", false),
+    ("White Manna Cheeseburger", true),
+    ("Kobe Burger", false),
+    ("Black Label Burger", true),
+    ("Steamed Cheeseburger", true),
+    ("Primetime Burger", false),
+    ("Kuma Burger", true),
+    ("Sirloin Burger", false),
+    ("The Doh! Nut Burger", false),
+    ("Peanut Buter Bacon Burger", true),
+    ("Hodad's Bacon Cheeseburger", true),
+    ("Zuni's Cafe Hamburger", false);
+   
+
     
 -- displays all burgers  
 SELECT * FROM burgers;
